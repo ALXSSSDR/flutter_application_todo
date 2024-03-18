01a348e781f14c9ca71b57669fedb8fa5a870146
Alexander <blue_chill@icloud.com>
2024-03-18 20:51:13 +1000

    Задание 1 - Экран категорий - Экран задач - Удаление задач и категорий - фильтры

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,31 +11,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ToDo App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 101, 221, 205),
+          centerTitle: true,
+          titleTextStyle: TextStyle(fontSize: 27, fontWeight: FontWeight.bold, color: Color.fromRGBO(0, 0, 0, 1)),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ToDo'),
     );
   }
 }
 
+class Category {
+  String id;
+  String name;
+
+  Category({required this.id, required this.name});
+}
+
+class Task {
+  String id;
+  String title;
+  bool isCompleted;
+  bool isFavourite;
+
+  Task({required this.id, required this.title, this.isCompleted = false, this.isFavourite = false});
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -42,71 +50,288 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<Category> _categories = [];
+  final List<Task> _tasks = [];
 
-  void _incrementCounter() {
+  void _addCategory(String categoryName) {
+    const uuid = Uuid();
+    final newCategory = Category(id: uuid.v4(), name: categoryName);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _categories.add(newCategory);
+    });
+  }
+
+  void _showAddCategoryDialog() {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        String categoryName = '';
+        return AlertDialog(
+          title: const Text('Добавить новую категорию'),
+          content: TextField(
+            onChanged: (value) {
+              categoryName = value;
+            },
+            autofocus: true,
+            decoration: const InputDecoration(hintText: "Название категории"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Отмена'),
+            ),
+            TextButton(
+              onPressed: () {
+                _addCategory(categoryName);
+                Navigator.pop(context, 'OK');
+              },
+              child: const Text('Добавить'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _removeCategory(String id) {
+    setState(() {
+      _categories.removeWhere((category) => category.id == id);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: _categories.isEmpty
+          ? const Center(
+              child: Text("Список категорий пуст",
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          : Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(9.0),
+                  child: Text(
+                    "Категории",
+                    style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                Expanded(
+                  
+                  child: ListView.builder(
+                    itemCount: _categories.length,
+                    itemBuilder: (context, index) {
+                      final category = _categories[index];
+                      return Dismissible(
+                        key: Key(category.id),
+                        onDismissed: (_) => _removeCategory(category.id),
+                        direction: DismissDirection.endToStart,
+                        background: Container(
+                          color: Colors.red,
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.only(right: 20.0),
+                        
+                          child: const Icon(Icons.delete, color: Colors.white),
+                        ),
+                        child: Card(
+                          margin: const EdgeInsets.only(left: 10, right: 10),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0), 
+                          ),
+                          child: ListTile(
+                            title: Text(category.name),
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => TasksPage(categoryName: category.name, tasks: _tasks)));
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddCategoryDialog,
+        tooltip: 'Добавить категорию',
+        backgroundColor: const Color.fromARGB(255, 127, 224, 216),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+// ignore: must_be_immutable
+class TasksPage extends StatefulWidget {
+  final String categoryName;
+  List<Task> tasks;
+  TasksPage({super.key, required this.categoryName, required this.tasks});
+
+  @override
+  State<TasksPage> createState() => _TasksPageState();
+}
+
+class _TasksPageState extends State<TasksPage> {
+  String _filterType = "Все";
+
+  List<Task> get _filteredTasks {
+    switch (_filterType) {
+      case "Завершенные":
+        return widget.tasks.where((task) => task.isCompleted).toList();
+      case "Незавершенные":
+        return widget.tasks.where((task) => !task.isCompleted).toList();
+      case "Избранные":
+        return widget.tasks.where((task) => task.isFavourite).toList();
+      default:
+        return widget.tasks;
+    }
+  }
+
+  void _addTask(String taskTitle) {
+    const uuid = Uuid();
+    final newTask = Task(id: uuid.v4(), title: taskTitle);
+    setState(() {
+      widget.tasks.add(newTask);
+    });
+  }
+  
+  void _toggleTaskCompletion(Task task) {
+    setState(() {
+      task.isCompleted = !task.isCompleted;
+    });
+  }
+  
+  void _toggleTaskFavourite(Task task) {
+    setState(() {
+      task.isFavourite = !task.isFavourite;
+    });
+  }
+
+  void _removeTask(String id) {
+    setState(() {
+      widget.tasks.removeWhere((task) => task.id == id);
+    });
+  }
+
+  void _showAddTaskDialog() {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        String taskTitle = '';
+        return AlertDialog(
+          title: const Text('Добавить новую задачу'),
+          content: TextField(
+            onChanged: (value) {
+              taskTitle = value;
+            },
+            autofocus: true,
+            decoration: const InputDecoration(hintText: "Название задачи"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Отмена'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (taskTitle.isNotEmpty) {
+                  _addTask(taskTitle);
+                  Navigator.pop(context, 'OK');
+                }
+              },
+              child: const Text('Добавить'),
             ),
           ],
-        ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.categoryName),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (String value) {
+              setState(() {
+                _filterType = value;
+              });
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem(value: "Все", child: Text("Все")),
+                const PopupMenuItem(value: "Завершенные", child: Text("Завершенные")),
+                const PopupMenuItem(value: "Незавершенные", child: Text("Незавершенные")),
+                const PopupMenuItem(value: "Избранные", child: Text("Избранные")),
+              ];
+            },
+          ),
+        ],
       ),
+      body: _filteredTasks.isEmpty
+          ? const Center(
+              child: Text('Пока что у вас нет задач'),
+            ) : 
+          Column(
+              children: [
+                 Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Text(
+                    '$_filterType задачи',
+                    style: const TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredTasks.length,
+              itemBuilder: (context, index) {
+                final task = _filteredTasks[index];
+                return Dismissible(
+                  key: Key(task.id),
+                  onDismissed: (_) => _removeTask(task.id),
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: const Icon(Icons.delete, color: Colors.white),
+                  ),
+                  child: Card(
+                    margin: const EdgeInsets.only(left: 10, right: 10),
+                    child: ListTile(
+                      title: Text(task.title),
+                      leading: IconButton(
+                        icon: Icon(task.isCompleted ? Icons.check_box : Icons.check_box_outline_blank),
+                        onPressed: () => _toggleTaskCompletion(task),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(task.isFavourite ? Icons.star : Icons.star_border, color: Colors.yellow[700]),
+                        onPressed: () => _toggleTaskFavourite(task),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            ),
+              ],
+          ),
+          
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _showAddTaskDialog,
+        tooltip: 'Добавить задачу',
+        backgroundColor: const Color.fromARGB(255, 127, 224, 216),
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
